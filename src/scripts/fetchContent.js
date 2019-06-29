b3cdfb678ac557084f6dd5e285663688c4db5a48
Rafael Pelle <rafapelle@gmail.com>
2019-06-29 03:57:46 +0000

    Implements the 'show next page' button...

--- src/scripts/fetchContent.js
+++ src/scripts/fetchContent.js
@@ -1,12 +1,19 @@
-fetch('https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1')
-	.then((response) => {
-		return response.json()
-	})
-	.then((data) => {
-		for(product of data.products) {
-			appendProductCard(product)
-		}
-	})
-	.catch((error) => {
-		console.log(error)
-	})
+let nextPage = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
+fetchProductPage(nextPage)
+
+
+function fetchProductPage(endpoint) {
+	fetch(endpoint)
+		.then((response) => {
+			return response.json()
+		})
+		.then((data) => {
+			nextPage = data.nextPage
+			for(product of data.products) {
+				appendProductCard(product)
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
